0d16811a0eaa7909c8839bdd210f1c519e28bec1
nekodev17 <nekodev17@proton.me>
2026-09-26 14:53:25 +0530

    holland1: cust -> vendor

--- recovery/root/system/bin/merge-fstab.sh
+++ recovery/root/system/bin/merge-fstab.sh
@@ -42,14 +42,14 @@ if dd if=/dev/block/by-name/system bs=256k count=1|strings|grep mi8937_dynpart >
         echo "${p} /${p} ext4 rw,barrier=1,discard wait,logical" >> /system/etc/recovery.fstab
     done
     echo >> /system/etc/twrp.flags
-    for p in cust; do
+    for p in vendor; do
         echo "/super_${p} emmc /dev/block/by-name/${p} flags=display=\"Super_${p}\";backup=1" >> /system/etc/twrp.flags
     done
 else
     echo >> /system/etc/twrp.flags
     cat /system/etc/twrp.flags.nondynpart >> /system/etc/twrp.flags
     echo "/super emmc /dev/block/by-name/system flags=display=\"Super\";backup=1" >> /system/etc/twrp.flags
-    for p in cust; do
+    for p in vendor; do
         echo "/super_${p} emmc /dev/block/by-name/${p} flags=display=\"Super_${p}\";backup=1" >> /system/etc/twrp.flags
     done
 fi
